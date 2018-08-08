2420a6b8a405ee8508fb9780488796be11c5690f
Gregory Brewster <gbrewster@ci.sandy.or.us>
2018-08-08 15:13:35 -0700

    meh

--- bin/my_init.d/postfix.sh
+++ bin/my_init.d/postfix.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-
-if ! [ "${MAIL_RELAY_HOST}" = "" ]; then
-    sed -i "s/relayhost =.*/relayhost = ${MAIL_RELAY_HOST}/" /etc/postfix/main.cf
-fi
-if ! [ "${MAIL_INET_PROTOCOLS}" = "" ]; then 
-    sed -i "s/inet_protocols =.*/inet_protocols = ${MAIL_INET_PROTOCOLS}/" /etc/postfix/main.cf
-fi
 sed -i "s/myhostname =.*/myhostname = $(hostname)/" /etc/postfix/main.cf
-sed -i "s/mydestination =.*/mydestination = ${FQDN}, \$myhostname, localhost.localdomain, localhost/" /etc/postfix/main.cf
+sed -i "s/mydestination =.*/mydestination = \$myhostname, localhost.localdomain, localhost/" /etc/postfix/main.cf
 
 sed -i "/^myorigin =.*/d" /etc/postfix/main.cf
-echo "${FQDN}" > /etc/mailname
+echo "\$myhostname" > /etc/mailname
 
 #postfix runs in a chroot and needs resolv.conf to resolve hostnames
 cp /etc/resolv.conf /var/spool/postfix/etc/resolv.conf
