f82fb75734254ef47ee6bfa1cb68cf64a9a36d8e
Gregory Brewster <gbrewster@ci.sandy.or.us>
2018-08-08 15:20:35 -0700

    asdf

--- bin/my_init.d/postfix.sh
+++ bin/my_init.d/postfix.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "*** Setting postfix vars..."
 sed -i "s/myhostname =.*/myhostname = $(hostname)/" /etc/postfix/main.cf
 sed -i "s/mydestination =.*/mydestination = \$myhostname, localhost.localdomain, localhost/" /etc/postfix/main.cf
 
@@ -7,6 +8,7 @@ sed -i "/^myorigin =.*/d" /etc/postfix/main.cf
 echo "\$myhostname" > /etc/mailname
 
 #postfix runs in a chroot and needs resolv.conf to resolve hostnames
+echo "*** Copying files for resolv.conf..."
 cp /etc/resolv.conf /var/spool/postfix/etc/resolv.conf
-
-exec /usr/lib/postfix/sbin/master -d -c /etc/postfix
+echo "*** Something something, postfix?..."
+/usr/lib/postfix/sbin/master -d -c /etc/postfix
